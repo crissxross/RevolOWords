b02315b1b0078cb3bb066b496d1e080769d16b3a
Christine Wilks <christine@Christines-MacBook-Pro.local>
2013-10-06 17:02:45 +0100

    Updated Revolution texts
    Added the latest texts and edits as listed and drafted in Randy's
    emails dated 4 Oct '13, which includes some new additions.

--- js/revol.js
+++ js/revol.js
@@ -9,67 +9,81 @@ $(function(){
         // REVOLUTION TEXTS
         theTexts = {
         freedom: [
-            "Six-hundred thousand Africans were enslaved within the boundaries of the continent, by other Africans, each year during the 19th century - about 10 times the number of slaves transported across the Atlantic.",
-            "\"War is Peace, Freedom is Slavery, Ignorance is Strength.\" George Orwell",
-            "We will fight to protect your freedom to die for causes we decide you are free to fight and die for.",
-            "We will promote your freedom to consume, even the things that will consume you. Have no fear. We will bathe you in an endless stream of messages that speak to your subconscious and liberate your desire to consume more.",
-            "The general population have had their liberties taken away from them, but they rather enjoy it. They are being distracted from revolution by \"propaganda disguised as entertainment.\" [#] Do you feel entertained? [Aldous Huxley]."
+            "Introducing the Freedom Rebate International Employment Program (FRIEP), a time tested employment strategy. The FRIEP indenture program is legal in most underdeveloped countries and our legal team keeps abreast of loopholes in the laws of other nations, where it has been erroneously suggested that we support slavery.",
+            "We will fight to protect your freedom. You will die for causes we decide for you. You will fight and die for the right reason. We will train your children to fight the good war. Freedom and glory await.",
+            "We will promote your freedom to consume. Have no fear. We will bathe you in an endless stream of messages that speak to your subconscious, freeing your desire to consume more.",
+            "Your liberties have been taken away from you in the name of Freedom. Enjoy the irony - be distracted from revolution by propaganda disguised as entertainment. Are you waiting for the next instalment of your favourite series? Do you feel entertained?",
+            "Take two Freedom-inducing tablets every evening before bed. Enjoy the drowsy effect. Wallow in the dream-free dullness. At the start of your workday, take the Freedom-plus tablet, be productive. When the day overwhelms, take your evening pill. Freedom through medication.",
+            "Our Freedom to Chose program will free you from the drudgery of making choices. Leave the little things to us and focus on what really matters.",
+            "Feel free to opt out at anytime."
         ],
         order: [
-            "Has your country been a subject or an object of history?",
-            "There is silence on all the important matters, we move from crisis to crisis as if happiness is a vice.",
-            "Opinion strikes like a gavel. That sound you hear is a hardwood mallet brought down in your brain to establish the next pronouncement. No discussion is necessary.",
-            "Racism, xenophobia, and superiority syndromes are terrific tools to bring people on-side. Corruption and violence will keep them on-message. Well-placed rumours will pay dividends. Buy into the program, or chose to live a drab life of desperation."
+            "Has your country been a subject or an object of history? Do you feel downtrodden, or emulated? Do you fit within the natural order of things? Take stock, it's never too late to take up arms. Order through conflict.",
+            "There is silence on all the important matters, we move from crisis to crisis as if happiness is a vice. Don't fret; things will be set right, far right. Order will be restored through corruption and threats. This is the natural order of things.",
+            "Opinion strikes like a gavel. That sound you hear is a hardwood mallet brought down in your brain to establish Order. No discussion is necessary.",
+            "Racism, xenophobia, and superiority syndromes are terrific tools to bring people on side. Corruption and violence will keep them on-message. Well-placed rumours will pay dividends. Buy into the program, or chose to live a drab life of desperation.",
+            "Order is everything. Join the Order of Orders today. The OoO is a blanket organization that guarantees you a place in line for what you really deserve. Act now or lose your place in the queue."
         ],
         natural: [
-            "We ignore the sheer, unharnessed energy of the oceans, while crawling about on the planet like ants carrying grains of sugar from a limited source.",
-            "The pre-given world of natural things has lost its relevance in this modern age. Some critics now say all things are natural, but this only applies if you are able to drop your morality and wait for the logical end of natural things. You won't have long to wait.",
-            "Natural Whole Foods Group Bulletin: It's only natural to pay a premium for Whole Foods because NWFG has created a lifestyle brand that delivers on its promise. Our product will monitor you constantly to ensure that you get the most benefit from an NWFG lifestyle."
+            "We ignore the sheer, unharnessed energy of the oceans, while crawling about on the planet like ants carrying grains of sugar from a limited source. Given that God put the oil in the ground, this is only natural.",
+            "The pre-given world of natural things has lost its relevance in this modern age. Some critics say all things are natural, but this only applies if you are able to drop your morality and wait for the logical end of natural things. You won't have long to wait.",
+            "Natural Whole Foods Group Bulletin: It's only natural to pay a premium for Whole Foods because NWFG has created a lifestyle brand that delivers on its promise. Our product will monitor you constantly to ensure you get the most benefit from the NWFG lifestyle."
         ],
         loyalty: [
             "He was loyal to the party and voted against his conscience, this was made possible by blind faith in the Leader.",
-            "I have a loyalty that runs in my bloodstream, when I lock into someone or something, you can't get me away from it. I commit thoroughly. That's in policy, a deal, a commitment. Right or wrong.",
+            "I have a loyalty that runs in my bloodstream, when I lock into someone or something, you can't get me away from it. I commit thoroughly. That's in policy, a deal, and a commitment. Loyalty, right or wrong.",
             "I'll take fifty percent efficiency to get one hundred percent loyalty. That's how you run a country.",
             "What history are you loyal to? Would you swear by a history? Does it include bones? Are you loyal to certain bones?",
-            "Loyalty is an important technology. Our loyalty program operators are ready to get you up and running with our innovative Smart Button. Loyalty is a click away.",
-            "One session with Dr Justin Love, our Loyalty Sessionist, and you will be rid of any instances of disloyalty, faithlessness, falseness, falsity, inconstancy, infidelity, perfidiousness, perfidy, treachery, or unfaithfulness. Certification guaranteed."
+            "Loyalty is an important technology. Our Loyalty program operators are ready to get you up and running with our innovative Smart Button. Loyalty is a click away.",
+            "One session with Dr Justin Love, our Loyalty Sessionist, and you will be rid of any instances of disloyalty, faithlessness, falseness, falsity, inconstancy, infidelity, perfidiousness, perfidy, treachery, or unfaithfulness. Certification guaranteed.",
+            "Revolutionaries and Fascists share a need for lackeys. Behind the zealots are lackeys who perform deeds to sustain the ideologies. If you are uncertain, use our nifty secure online scripts to choose your ideology. We have a secure sign-up program so you can begin action today. Lackeys are the future."
         ],
         justice: [
             "A rich man in Thailand harbored his murderous son in a garden of peacocks and palms.",
-            "A system of justice can execute an innocent person. This makes of Justice a lottery system, not a system of justice."
+            "A system of justice can execute an innocent person. This makes of Justice a lottery system, not a system of justice.",
+            "Be honest: would you prefer justice or wealth? Would immense wealth be balm to sooth all memory of pain or anger?"
         ],
         truth: [
-            "What is Truth if not put to the test? Is Truth a comfortable chair and a fire in the stove? That is comfort. Truth is in the fire.",
-            "It might be said that the meaning of one word is always another word. Apply this to Truth, or someone else will do it for you.",
+            "What is Truth if not put to the test? Truth does not come with a comfortable chair and a fire in the stove, snuggled up with a Good Book? That is comfort. Truth is in the fire.",
+            "It might be said that the meaning of one word is always another word. Apply this to Truth, or someone will do it for you.",
             "There is a hard kernel of Truth at the core, under our Plasticine lives. We can re-model as we wish, as we age, during trauma or glee - but the only way to reach the core is by stripping away the clay.",
             "The Competition Bureau prohibits making any deceptive representations for the purpose of promoting a product or a business interest. The provision does not apply if the advertised representation was a mistake. We all make mistakes.",
-            "\"You can be sure of this, fellow scribbler. You can trust the darkness when the light lies.\" Eugenio Montale",
-            "He waved the truth around and said he had proof, but at the end of the day he could not provide the truth. There is no such thing as a half-truth - is there?",
-            "\"Amongst the many bearded prophets wandering around someone might even have touched the truth but only with his finger which he then withdrew. The truth burns.\" Eugenio Montale",
-            "\"The truth lies in the nibblings of moths and mice, in the powder that issues from moldy chests of drawers, n the crusts of seasoned Parmesan.\" Eugenio Montale"
+            "Truth can not withstand the light of day. Truth is generally unbearable. Don't stumble about in the dark; try our specially designed Seeker glasses. They will protect you from the darkest of Truths.",
+            "He waved the Truth around and said he had proof, but at the end of the day he could not provide all the proofs. There is no such thing as partly true - is there?",
+            "Maybe a few of the many bearded prophets who wander the planet have found the Truth. You can identify them by their burnt fingertips. Approach with caution, lest you get scorched.",
+            "Truth resides in your gut. Truth is barely edible. It is indigestible. Our revolutionary Truth Pills enable you to stomach the truth. Free samples are available on the street corner, where all good drugs are sold.",
+            "Act now on our special offer. Are you finding the Truth to be boring? Try our handy spice to pep up the Truth. It is offered in several subtle flavours and comes with a No Truth guarantee. Perfect for parties or election campaigns.",
+            "History and slogans are most always at odds. Although History attempts to reveal the Truth of human matters, slogans will grab and hold the public attention. Trust in our Slogan Generator to veil the Truth of anything. Act now and receive a free Proof Slicer."
         ],
         equality: [
-            "We carry our differences to the grave.",
-            "\"What travel discloses to us first of all is our garbage, flung in the face of humanity.\" Paul Bowles",
-            "\"The revolution of the word is the revolution of the world, and that both cannot exist without the revolution of the body: life as art, a return to the mythic lost unity of thought and body, man and nature, I and the other.\" Octavio Paz"
+            "What did you expect? Equality? Nothing equals nothing else. There is never enough time to balance the odds. Mortality draws the line. We carry our differences to the grave.",
+            "The Garbage Equality International mandate reads: We guarantee that all nations will share equally in garbage disposal - based on population. A recent report states that over-populated areas should learn to use garbage as fuel and building material. GEI social programs include training sessions. Critics believe the sessions are redundant.",
+            "The Revolution of the Word is a worldwide organization. We are running a contest, open to all nationalities. Send us any word that you would like to revolutionize, such as Equality. We will give it a revolting meaning.",
+            "Equality is a new breakfast cereal enjoyed by young and old alike. Pour a bowl of Equality every morning for a balanced diet of facts and lies. Equality is guaranteed to stick to your convictions.",
+            "Join the Equality Network, where equal rights are negotiable. Shuck your chains. Stay on top of daily events that affect your rights. Learn to spread equality through our worldwide network of Slave Technologists.",
+            "Play Equality, a new international role-playing game. This time-line based game lets you develop characters including slave-traders, stockbrokers, revolutionaries, or activists. The Level Playing Field function is still under development."
         ],
         hope: [
-            "The athlete jogs to the hangman.",
             "Hope Springs INC: for all your suspension needs. Suspend grief, suspend worry, suspend anger, suspend lies. Every tune-up includes a minimum 150 Hope Points. Bonus points if you upgrade to the Eternal Club: suspend yourself in hope.",
-            "Peddling the semblance of hope, every lottery ticket bought inflates idle wishes, devaluing any firm grounds for optimism. This is a guarantee."
+            "Peddling the semblance of hope, every lottery ticket bought inflates idle wishes, devaluing any firm grounds for optimism. This is a guarantee.",
+            "Have you ever hoped for things to be different? Our program offers Hope. Remember your loved ones at a fraction of the cost of a real relationship. Available as gift subscriptions. We restore lost Love. Act now!",
+            "Take one of our Hope pills and forget your worries. Live in a dreamland of hope where everything is possible. Experience the joy of forgetfulness. Hope Pharmacies makes the world a better place."
         ],
         security: [
-            "The drone pilot shifted in his seat. There was a flash on the screen: the explosion. Parts of the building collapsed. The child disappeared. Paraphrased from Mattathias Schwartz, LRB.",
-            "In a signature strike, a person is made a target not because of their identity but because of certain signatures - codified justifications for killings that have already taken place. Paraphrased from Mattathias Schwartz, LRB.",
-            "In today's day and age, every computer should be protected. It is best to wear a tin-foil hat while browsing the web. Maybe also surround your computer with talismans."
+            "The drone pilot shifted in his seat. There was a flash on the screen: the explosion. Parts of the building collapsed. The child disappeared. One less terrorist in the making.",
+            "In a signature strike, a person is made a target not because of their identity but because of certain signatures - codified justifications for killings in a secure world.",
+            "In today's day and age, every computer should be protected. It is best to wear a tin-foil hat while browsing the web. Maybe also surround your computer with talismans or lit candles. Feel secure.",
+            "Feel secure in the knowledge that Home Security is working for you. Our program of drone attacks and targeted assassinations is making your world more secure. Stay glued to your screen and watch from the safety of your home."
         ],
         value: [
-            "\"From the very beginning of the modern age the history, morality, and politics of the West have been grounded in an overvaluing of the future.\" Octavio Paz",
-            "Language is the heaviest of burdens. Poor translation can be costly.",
-            "This moment is worth all of your futures.",
-            "Charity grows coldest \"in those which most doe manifest the fires and flames of zeale.\" Thomas Browne, 1605 - 1682",
-            "Henry James warned the graduates about destroying the \"ancestral circle\" of language, and making it instead \"a mere helpless slobber of disconnected vowel noises.\"",
-            "\"There are worlds composed of nonsensical trifles that have great meaning, and worlds composed of apparently serious matters that have no meaning at all.\" Vasily Grossman"
+            "Don't fall into the trap of over-valuing the future. Don't trade your present for an uncertain future. Use our online Modern Age Value System for everyday choices. Trust us to ground you in the present. Contact us now. This is a limited-time offer.",
+            "Language is the heaviest of burdens. Poor translation can be costly. Don't trust anyone but us to translate your desires and goals. Speak the language of your country. Value purity.",
+            "This moment is worth all of your futures. Monetize your present for full value using our nifty new Value Added generator. Make your profile work for you.",
+            "Value human rights. Struggle against the self-righteous whose intolerance place ideals above people. Beware of the cold fire that manifests in zealots. Be charitable. Be human.",
+            "Language is under threat. Meaning is losing meaning. We offer a helpful guide to what things really mean. Don't contribute to the \"slobber of disconnected vowel noises.\" Be meaningful.",
+            "Value International is an online organization that can help people order their values. Values can be traded or changed. Nonsensical trifles or serious matters are given equal weight. Values can be stored for later use, or donated as gifts.",
+            "Value speed. Be ecstatic. Keep up with technology. For a premier experience, update and upgrade daily. Follow the instructions, you have nothing to lose.",
+            "All of our products include value-added gifts. Fill out our online questionnaire so we can serve Values that are custom-designed to your needs and desires. Trust our tasty cookies."
         ]
     },
         winWords = ["natural","order","freedom","value","security","hope","equality","truth","justice","loyalty"],
